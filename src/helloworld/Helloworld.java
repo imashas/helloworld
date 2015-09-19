22dea8749b6eb9332e9815e227192a96806ac78a
Madawa Soysa <madawa.rc@gmail.com>
2015-09-19 15:06:12 +0530

    change print value

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -16,7 +16,7 @@ public class Helloworld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hiii");
+        System.out.println("Test test");
     }
     
 }
